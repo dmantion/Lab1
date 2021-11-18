43cdc610b20aeabbfc17f6ffebb035e1227a6982
dmantion <devilanimation666@gmail.com>
2021-11-18 14:14:22 +0700

    sdelal 2 labu, dovolen schetchik sdelan 4ka v karmane

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int s = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view)
+    {
+        TextView textView = findViewById(R.id.text1);
+        textView.setText("На кнопку нажали - " + s++ );
+    }
+
+}
